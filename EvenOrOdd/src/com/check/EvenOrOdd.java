ac54020c087815978d55db14f203fecd7bbb4d1c
Mahendra <bmc26g@gmail.com>
2022-07-07 15:34:21 +0530

    I used if else.

---
+++ EvenOrOdd/src/com/check/EvenOrOdd.java
@@ -0,0 +1,29 @@
+package com.check;
+
+import java.util.Scanner;
+
+public class EvenOrOdd {
+
+	public static void main(String[] args) {
+		
+		Scanner reader = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = reader.nextInt();
+
+        if(num % 2 == 0)
+            System.out.println(num + " is even");
+        else
+            System.out.println(num + " is odd");
+        
+        reader.close();
+        
+
+	}
+
+}
+
+
+//String evenOdd = (num % 2 == 0) ? "even" : "odd";
+
+//System.out.println(num + " is " + evenOdd);
