f66d31ffac429419f0b3da11d5d42e83534b5a58
BattaMahendra <97429831+BattaMahendra@users.noreply.github.com>
2022-07-08 09:57:06 +0530

    Update EvenOrOdd.java

--- EvenOrOdd/src/com/check/EvenOrOdd.java
+++ EvenOrOdd/src/com/check/EvenOrOdd.java
@@ -1,28 +1,5 @@
-package com.check;
+
 
 import java.util.Scanner;
 
-public class EvenOrOdd {
 
-	public static void main(String[] args) {
-		
-		Scanner reader = new Scanner(System.in);
-
-        System.out.print("Enter a number: ");
-        int num = reader.nextInt();
-
-       String evenOdd = (num % 2 == 0) ? "even" : "odd";
-
-       System.out.println(num + " is " + evenOdd);
-        
-        reader.close();
-        
-
-	}
-
-}
-
-
-//String evenOdd = (num % 2 == 0) ? "even" : "odd";
-
-//System.out.println(num + " is " + evenOdd);
